c0b9f124ef4a7db5d0d1b353bbdeb739b0c6f758
MaCHINA <konstload@gmal.com>
2022-11-16 19:57:17 +0300

    Примичание курьеру на странице офрмления заявки, отображение коментария к адресу

--- app/src/main/java/ru/iwater/youwater/screen/basket/CreateOrderFragment.kt
+++ app/src/main/java/ru/iwater/youwater/screen/basket/CreateOrderFragment.kt
@@ -1,14 +1,14 @@
 package ru.iwater.youwater.screen.basket
 
-import android.app.AlertDialog
 import android.os.Bundle
-import android.os.Message
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -20,9 +20,10 @@ import ru.iwater.youwater.base.BaseFragment
 import ru.iwater.youwater.data.*
 import ru.iwater.youwater.databinding.FragmentCreateOrderBinding
 import ru.iwater.youwater.screen.adapters.OrderProductAdapter
+import ru.iwater.youwater.screen.dialog.AddAddressDialog
+import ru.iwater.youwater.screen.dialog.AddNoticeDialog
 import timber.log.Timber
 import java.text.SimpleDateFormat
-import java.time.Year
 import java.util.*
 import javax.inject.Inject
 
@@ -30,7 +31,7 @@ import javax.inject.Inject
  * Фрагмент оформления заказа
  */
 class CreateOrderFragment : BaseFragment(),
-    DatePickerDialog.OnDateSetListener {
+    DatePickerDialog.OnDateSetListener, AddNoticeDialog.AddNoticeDialogListener, AddAddressDialog.ChoiceAddressDialog {
 
     @Inject
     lateinit var factory: ViewModelProvider.Factory
@@ -39,7 +40,22 @@ class CreateOrderFragment : BaseFragment(),
 
     private val productClear = mutableListOf<Product>()
     private var order =
-        Order(0, 0, "", mutableListOf(), "", 0, "", "", 0, "", "", "", JsonObject(), "")
+        Order(
+            clientId = 0,
+            acqOrderId = 0,
+            notice = "",
+            waterEquip = mutableListOf(),
+            period = "",
+            orderCost = 0,
+            paymentType = "",
+            status = 0,
+            email = "",
+            contact = "",
+            date = "",
+            addressId = 0,
+            name = "",
+            dateCreate =  java.sql.Date(Calendar.getInstance().timeInMillis)
+        )
 
     private val binding: FragmentCreateOrderBinding by lazy {
         FragmentCreateOrderBinding.inflate(
@@ -59,7 +75,8 @@ class CreateOrderFragment : BaseFragment(),
         // показывать сообщение о неудачной оплате:
         // true - да, false - нет
         val isShowMessage = CreateOrderFragmentArgs.fromBundle(this.requireArguments()).isShowMessage
-        warningPay(isShowMessage, "Извините оплатить заказ не удалось, попробуйте выбрать другой тип оплаты или отредактировать заказ")
+        val lastOrder = CreateOrderFragmentArgs.fromBundle(this.requireArguments()).lastOrderId
+        warningPay(isShowMessage)
 
         binding.lifecycleOwner = this
         /**
@@ -74,24 +91,33 @@ class CreateOrderFragment : BaseFragment(),
             if (it.email.isNotEmpty()) order.email = it.email
         }
 
+        viewModel.getInfoLastOrder(lastOrder)
+
         // выбор адреса доставки
         viewModel.rawAddress.observe(viewLifecycleOwner) { listRawAddress ->
             if (!listRawAddress.isNullOrEmpty()) {
-                binding.tvAddressOrder.text = "Выбрать адрес доставки"
+                for (rawAddress in listRawAddress) {
+                    Timber.d("Note in address ${rawAddress.id} ${rawAddress.notice}")
+                }
+                binding.btnSetAddress.text = "Выбрать адрес доставки"
                 val listAddress = mutableListOf<Address>()
                 val addresses = mutableListOf<String>()
                 listRawAddress.forEach { rawAddress ->
                     val region = rawAddress.fullAddress.split(",")[0]
                     addresses.add(rawAddress.factAddress)
-                    listAddress.add(viewModel.getAddressFromString(rawAddress.factAddress.split(","), region, rawAddress.id))
+                    listAddress.add(viewModel.getAddressFromString(rawAddress.factAddress.split(","), region, rawAddress.id, rawAddress.notice))
                 }
-                binding.tvAddressOrder.setOnClickListener {
-                    choiceAddressDialog(listAddress, addresses)
+                binding.btnSetAddress.setOnClickListener {
+                    listAddress.forEach {
+                        Timber.d("List Address ${it.note}")
+                    }
+                    AddAddressDialog.getAddressDialog(childFragmentManager, listAddress, addresses)
                 }
             } else {
                 // если адресов нету переводим на страницу создания адреса
-                binding.tvAddressOrder.text = "Добавить адрес"
-                binding.tvAddressOrder.setOnClickListener {
+                binding.btnSetAddress.text = "Добавить адрес"
+//                binding.tvAddressOrder.setBackgroundColor(Color.WHITE)
+                binding.btnSetAddress.setOnClickListener {
                     this.findNavController().navigate(
                         CreateOrderFragmentDirections.actionCreateOrderFragmentToAddAddressFragment(true)
                     )
@@ -102,34 +128,19 @@ class CreateOrderFragment : BaseFragment(),
         }
 
         //выбор даты заказа
-        binding.tvTimeOrder.text = "Укажите дату"
-        binding.tvTimeOrder.setOnClickListener {
+        binding.btnSetTime.text = "Укажите дату"
+        binding.btnSetTime.setOnClickListener {
             val calendar = Calendar.getInstance()
-            val year = calendar.get(Calendar.YEAR)
-            val month = calendar.get(Calendar.MONTH)
-            val day = calendar.get(Calendar.DAY_OF_MONTH)
-            val hour = calendar.get(Calendar.HOUR_OF_DAY)
-            //Установить дату
-            val datePickerDialog = DatePickerDialog.newInstance(this, year, month, day)
-            datePickerDialog.setTitle("Укажите время заказа")
-
-            if (hour >= 14) {
-                calendar.set(year, month, day + 1)
+            if (order.date.isNotEmpty()) {
+                val year = order.date.split('-')[0].toInt()
+                val month = order.date.split('-')[1].toInt() - 1
+                val day = order.date.split('-')[2].toInt()
+                Timber.d("DATE = $year, $month, $day")
+                calendar.set(year, month, day)
+                getCalendar(calendar)
+            } else {
+                getCalendar(calendar)
             }
-            // установить мин дату на сегодня
-            val minDate = Calendar.getInstance()
-            datePickerDialog.minDate = minDate
-
-            // Установить max дату year + 2
-            val maxDate = Calendar.getInstance()
-            maxDate.set(Calendar.YEAR, year + 2)
-            datePickerDialog.maxDate = maxDate
-
-            disableSunday(minDate, maxDate, datePickerDialog)
-
-            datePickerDialog.minDate = calendar
-            datePickerDialog.firstDayOfWeek = Calendar.MONDAY
-            datePickerDialog.show(parentFragmentManager, "SetDateDialog")
         }
         // детали заказа
         val adapterOrder = OrderProductAdapter()
@@ -223,38 +234,72 @@ class CreateOrderFragment : BaseFragment(),
                 spinnerAdapter.notifyDataSetChanged()
             }
         }
+
+        binding.cvNoticeOrder.setOnClickListener {
+            AddNoticeDialog.getAddNoticeDialog(childFragmentManager, order.notice)
+        }
+
+        viewModel.myOrder.observe(viewLifecycleOwner) { myOrder ->
+            if (myOrder != null) {
+                when (myOrder.payment_type) {
+                    "0" -> binding.spinnerPaymentType.setSelection(1)
+                    "2" -> binding.spinnerPaymentType.setSelection(2)
+                    "4" -> binding.spinnerPaymentType.setSelection(0)
+                    else -> {}
+                }
+                if (myOrder.notice.isNotEmpty()) {
+                    order.notice = myOrder.notice
+                    binding.tvOrderNotice.text = myOrder.notice
+                }
+            }
+        }
+
+        viewModel.address.observe(viewLifecycleOwner) { address ->
+            if (address != "Адрес устарел, выберете другой") {
+                binding.btnSetAddress.text = address
+            } else {
+                binding.btnSetAddress.text = address
+            }
+        }
+
         binding.btnCreateOrder.setOnClickListener {
             Timber.d("PERIOD ${order.period}")
-            if (binding.tvAddressOrder.text != "Выбрать адрес доставки" &&
-                binding.tvTimeOrder.text != "Укажите дату" &&
+            if (binding.btnSetAddress.text != "Выбрать адрес доставки" &&
+                binding.btnSetAddress.text != "Адрес устарел, выберете другой" &&
+                binding.btnSetTime.text != "Укажите дату" &&
                 order.period.isNotEmpty() &&
+                order.period != "**:**-**:**" &&
                 order.paymentType == "4") {
-                viewModel.sendAndSaveOrder(order, product, order.address)
+                viewModel.sendAndSaveOrder(order)
                 createOrder(viewModel)
 
 
             }
 
-            else if (binding.tvAddressOrder.text != "Выбрать адрес доставки" &&
-                binding.tvTimeOrder.text != "Укажите дату" &&
+            else if (binding.btnSetAddress.text != "Выбрать адрес доставки" &&
+                binding.btnSetAddress.text != "Адрес устарел, выберете другой" &&
+                binding.btnSetTime.text != "Укажите дату" &&
                 order.period.isNotEmpty() &&
+                order.period != "**:**-**:**" &&
                 order.paymentType == "0") {
-                viewModel.sendAndSaveOrder(order, product, order.address)
+                viewModel.sendAndSaveOrder(order)
                 createOrder(viewModel)
             }
 
-            else if (binding.tvAddressOrder.text != "Выбрать адрес доставки" &&
-                binding.tvTimeOrder.text != "Укажите дату" &&
+            else if (binding.btnSetAddress.text != "Выбрать адрес доставки" &&
+                binding.btnSetAddress.text != "Адрес устарел, выберете другой" &&
+                binding.btnSetTime.text != "Укажите дату" &&
                 order.period.isNotEmpty() &&
+                order.period != "**:**-**:**" &&
                 order.paymentType == "2") {
-                viewModel.sendAndSaveOrder(order, product, order.address)
+                viewModel.sendAndSaveOrder(order)
                 viewModel.statusOrder.observe(this.viewLifecycleOwner) { status ->
                     when(status) {
                         Status.SEND -> {
                             viewModel.numberOrder.observe(this.viewLifecycleOwner) { numberOrder ->
                                 viewModel.payToCard(numberOrder, amount = order.orderCost * 100, order.contact)//*100
                                 viewModel.dataPayment.observe(this.viewLifecycleOwner) { dataPayment ->
-                                    Timber.d("SBER LINK: ${dataPayment[0]}; ${dataPayment[1]}")
+//                                    Timber.d("SBER LINK: ${dataPayment[0]}; ${dataPayment[1]}")
                                     val orderId = dataPayment[0].removePrefix("\"").removeSuffix("\"")
                                     val url = dataPayment[1].removePrefix("\"").removeSuffix("\"")
                                     this.findNavController().navigate(CreateOrderFragmentDirections.actionCreateOrderFragmentToCardPaymentFragment(url, orderId))
@@ -274,11 +319,54 @@ class CreateOrderFragment : BaseFragment(),
         return binding.root
     }
 
+    // добавить примичание
+    override fun onDialogAddNotice(inputNotice: String, dialog: DialogFragment) {
+        if (TextUtils.isEmpty(inputNotice)){
+            Toast.makeText(this.context, "Примичание не было добавлено", Toast.LENGTH_LONG).show()
+            dialog.dismissNow()
+        } else {
+            order.notice = inputNotice
+            binding.tvOrderNotice.text = inputNotice
+            dialog.dismissNow()
+        }
+    }
+
+    //выбрать адрес
+    override fun choiceAddress(
+        dialogFragment: DialogFragment,
+        id: Int?,
+        addressString: String?,
+        notice: String?
+    ) {
+        binding.btnSetAddress.text = addressString
+        if (id != null) {
+            order.addressId = id
+        }
+        if (notice != null) {
+            binding.cvNoticeAddress.visibility = View.VISIBLE
+            binding.tvAddressNotice.text = notice
+        } else {
+            binding.cvNoticeAddress.visibility = View.GONE
+        }
+        dialogFragment.dismissNow()
+    }
+
+    // отмена выбора адреса
+    override fun cancelClick(dialogFragment: DialogFragment) {
+        binding.btnSetAddress.text = "Выбрать адрес доставки"
+        order.addressId = 0
+        dialogFragment.dismissNow()
+    }
+
+    // отмена добавления коментария
+    override fun onDialogCancelNotice(dialog: DialogFragment) {
+        dialog.dismissNow()
+    }
+
     private fun createOrder(viewModel: OrderViewModel) {
         viewModel.statusOrder.observe(this.viewLifecycleOwner) { status ->
             when (status) {
                 Status.SEND -> {
-//                    viewModel.clearProduct(productClear)
                     viewModel.numberOrder.observe(this.viewLifecycleOwner) { numberOrder ->
                             warning("Заявка отправлена")
                         this.findNavController().navigate(
@@ -343,7 +431,7 @@ class CreateOrderFragment : BaseFragment(),
             "19:00-22:00",
             "**:**-**:**"
         )
-        if (monthOfYear + 1 > 10) "$dayOfMonth.${monthOfYear + 1}.$year".also {
+        if (monthOfYear + 1 >= 10) "$dayOfMonth.${monthOfYear + 1}.$year".also {
             setTimeOrderText(it)
             choiceDateAndTime(year, monthOfYear, dayOfMonth, todayTimes, saturdayTimes, otherTimes)
         } else "$dayOfMonth.0${monthOfYear + 1}.$year".also {
@@ -352,29 +440,35 @@ class CreateOrderFragment : BaseFragment(),
         }
     }
 
-    // выбор адреса доставки
-    private fun choiceAddressDialog(listAddress: List<Address>, addressesString: List<String>) {
-        AlertDialog.Builder(requireContext())
-            .setSingleChoiceItems(addressesString.toTypedArray(), -1) {
-                    _, witch ->
-                binding.tvAddressOrder.text = addressesString[witch]
-                order.addressJson.apply {
-                    addProperty("region", listAddress[witch].region)
-                    addProperty("street", listAddress[witch].street)
-                    addProperty("house", listAddress[witch].house)
-                    addProperty("building", listAddress[witch].building)
-                    addProperty("entrance", listAddress[witch].entrance)
-                    addProperty("floor", listAddress[witch].floor)
-                    addProperty("flat", listAddress[witch].flat)
-                }
-//                addressString = addressesString[witch]
-                order.address = addressesString[witch]
-            }
-            .setTitle("Выберете адрес")
-            .setPositiveButton(R.string.yes) { dialogInterface, _ ->
-                dialogInterface.cancel()
-            }
-            .create().show()
+    private fun getCalendar(calendar: Calendar) {
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        //Установить дату
+        val datePickerDialog = DatePickerDialog.newInstance(this, year, month, day)
+        datePickerDialog.setTitle("Укажите время заказа")
+
+        val minDate = Calendar.getInstance()
+        datePickerDialog.minDate = minDate
+
+        if (hour >= 14) {
+            val yearMin = minDate.get(Calendar.YEAR)
+            val monthMin = minDate.get(Calendar.MONTH)
+            val dayMin = minDate.get(Calendar.DAY_OF_MONTH)
+            minDate.set(yearMin, monthMin, dayMin + 1)
+            datePickerDialog.minDate = minDate
+        }
+        // установить мин дату на сегодня
+        // Установить max дату year + 2
+        val maxDate = Calendar.getInstance()
+        maxDate.set(Calendar.YEAR, year + 2)
+        datePickerDialog.maxDate = maxDate
+
+        disableSunday(minDate, maxDate, datePickerDialog)
+
+        datePickerDialog.firstDayOfWeek = Calendar.MONDAY
+        datePickerDialog.show(parentFragmentManager, "SetDateDialog")
     }
 
     // выбор даты и времени доставки
@@ -382,7 +476,7 @@ class CreateOrderFragment : BaseFragment(),
         val calendar = Calendar.getInstance()
         //дата заказа
         calendar.set(year, monthOfYear, dayOfMonth)
-        order.date = "${calendar.timeInMillis / 1000}"
+        order.date = SimpleDateFormat("yyyy-MM-dd").format(Date(calendar.timeInMillis))
         // дата заказа, день.месяц
         val dayOrder = SimpleDateFormat("dd.MM").format(Date(calendar.timeInMillis))
         // день недели
@@ -422,21 +516,21 @@ class CreateOrderFragment : BaseFragment(),
 
     //Отключить все ВОСКРЕСЕНЬЯ между минимальной и максимальной датами
     private fun disableSunday(minDate: Calendar, maxDate: Calendar, datePickerDialog: DatePickerDialog) {
-        var loopdate = minDate
+        var loopDate = minDate
         while (minDate.before(maxDate)) {
-            val dayOfWeek = loopdate[Calendar.DAY_OF_WEEK]
+            val dayOfWeek = loopDate[Calendar.DAY_OF_WEEK]
             if (dayOfWeek == Calendar.SUNDAY) {
                 val disabledDays = arrayOfNulls<Calendar>(1)
-                disabledDays[0] = loopdate
+                disabledDays[0] = loopDate
                 datePickerDialog.disabledDays = disabledDays
             }
             minDate.add(Calendar.DATE, 1)
-            loopdate = minDate
+            loopDate = minDate
         }
     }
 
     private fun setTimeOrderText(time: String) {
-        binding.tvTimeOrder.text = time
+        binding.btnSetTime.text = time
         binding.tvTimeSetLogo.visibility = View.VISIBLE
         binding.cvTimeSet.visibility = View.VISIBLE
     }
@@ -454,9 +548,9 @@ class CreateOrderFragment : BaseFragment(),
         Toast.makeText(this.context, message, Toast.LENGTH_LONG).show()
     }
 
-    private fun warningPay(isShowMessage: Boolean, message: String) {
+    private fun warningPay(isShowMessage: Boolean) {
         if (isShowMessage) {
-            warning(message)
+            warning("Извините оплатить заказ не удалось, попробуйте выбрать другой тип оплаты или отредактировать заказ")
         }
     }
 }
---
+++ app/src/main/java/ru/iwater/youwater/screen/dialog/AddAddressDialog.kt
@@ -0,0 +1,79 @@
+package ru.iwater.youwater.screen.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import ru.iwater.youwater.R
+import ru.iwater.youwater.data.Address
+import timber.log.Timber
+
+class AddAddressDialog : DialogFragment() {
+
+    internal lateinit var listAddress: List<Address>
+    internal lateinit var addressesString: List<String>
+    internal lateinit var listener: ChoiceAddressDialog
+
+    interface ChoiceAddressDialog {
+        fun choiceAddress(dialogFragment: DialogFragment, id: Int?, addressString: String?, notice: String?)
+        fun cancelClick(dialogFragment: DialogFragment)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dialog?.window?.setBackgroundDrawableResource(R.drawable.round_corner)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            var id: Int? = null
+            var addressString = ""
+            var notice: String? = "1"
+            builder.setTitle("Выберете адрес")
+                .setIcon(R.drawable.ic_address)
+                .setSingleChoiceItems(addressesString.toTypedArray(), -1) {
+                    _, witch ->
+                    id = listAddress[witch].id
+                    addressString = addressesString[witch]
+                    notice = listAddress[witch].note
+                    Timber.d("NOTICE + $notice")
+                }
+                .setPositiveButton("Выбрать") { _, which ->
+                    listener.choiceAddress(this, id, addressString, notice)
+                }
+                .setNegativeButton("Отмена") { _,_ ->
+                    listener.cancelClick(this)
+                }
+            builder.create()
+        } ?: throw IllegalStateException ("Activity cannot be null")
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            listener = parentFragment as ChoiceAddressDialog
+        } catch (e: ClassCastException) {
+            Timber.e("Error dialog: $e")
+        }
+    }
+
+    companion object {
+        private const val TAG = "AddAddress"
+        fun getAddressDialog(childFramentManager: FragmentManager, listAddress: List<Address>, addressString: List<String>) {
+            val dialog = AddAddressDialog()
+            dialog.listAddress = listAddress
+            dialog.addressesString = addressString
+            dialog.show(childFramentManager, TAG)
+        }
+    }
+}
