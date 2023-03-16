6b8aeb12e43bac1226f0afd68f90f451d35204c9
MaCHINA <konstload@gmal.com>
2023-03-16 19:12:48 +0300

    Методы для получения цены из продукта

--- app/src/main/java/ru/iwater/youwater/data/Product.kt
+++ app/src/main/java/ru/iwater/youwater/data/Product.kt
@@ -24,4 +24,45 @@ data class Product(
     val site: Int,
     var count: Int = 0,
     var onFavoriteClick: Boolean = false
-)
+) {
+
+    fun getMinPriceProduct(): Int {
+        return this.price.split(';')[0].split(':')[1].toInt()
+    }
+
+    fun getPriceOnCount(count: Int): Int {
+        val priceList = this.price.removeSuffix(";").split(';')
+        var price = 0
+        return when (this.id) {
+            81 -> {
+                priceList.forEach {
+                    val priceCount = it.split(':')
+                    if(priceCount[0].toInt() <= count) {
+                        price = (priceCount[1].toInt() - 15) * count
+                    }
+                }
+                price
+            }
+            84 -> {
+                priceList.forEach {
+                    val priceCount = it.split(':')
+                    if(priceCount[0].toInt() <= count) {
+                        price = (priceCount[1].toInt() - 15) * count
+                    }
+                }
+                price
+            }
+            else -> {
+                priceList.forEach {
+                    val priceCount = it.split(':')
+                    if(priceCount[0].toInt() <= count) {
+                        price = (priceCount[1].toInt()) * count
+                    }
+                }
+                price
+            }
+        }
+    }
+
+
+}
