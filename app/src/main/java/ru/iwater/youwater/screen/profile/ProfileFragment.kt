5e825acf6be6c4e568b8a86d7260e5cb982e0743
MaCHINA <konstload@gmal.com>
2023-04-21 09:54:06 +0300

    Profile Screen

--- app/src/main/java/ru/iwater/youwater/screen/profile/ProfileFragment.kt
+++ app/src/main/java/ru/iwater/youwater/screen/profile/ProfileFragment.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import ru.iwater.youwater.base.App
 import ru.iwater.youwater.base.BaseFragment
 import ru.iwater.youwater.data.ClientProfileViewModel
 import ru.iwater.youwater.databinding.FragmentProfileBinding
+import ru.iwater.youwater.theme.YourWaterTheme
 import javax.inject.Inject
 
 class ProfileFragment : BaseFragment() {
@@ -32,36 +34,16 @@ class ProfileFragment : BaseFragment() {
     ): View {
         val binding = FragmentProfileBinding.inflate(inflater)
         binding.lifecycleOwner = this
-        binding.clientVM = viewModel
-        binding.btnMyData.setOnClickListener {
-            this.findNavController().navigate(
-                ProfileFragmentDirections.actionProfileFragmentToUserDataFragment(false)
+        val navController = NavHostFragment.findNavController(this)
+        binding.composeViewProfileMenuScreen.apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.Default
             )
-        }
-        binding.btnAddress.setOnClickListener {
-            this.findNavController().navigate(
-                ProfileFragmentDirections.actionProfileFragmentToAddresessFragment()
-            )
-        }
-        binding.btnBankCard.setOnClickListener {
-            this.findNavController().navigate(
-                ProfileFragmentDirections.actionProfileFragmentToBankCardFragment()
-            )
-        }
-        binding.btnNotification.setOnClickListener {
-            this.findNavController().navigate(
-                ProfileFragmentDirections.actionProfileFragmentToNotificationFragment()
-            )
-        }
-        binding.btnMyFavorite.setOnClickListener {
-            this.findNavController().navigate(
-                ProfileFragmentDirections.actionProfileFragmentToFavoriteFragment()
-            )
-        }
-        binding.btnMyOrder.setOnClickListener {
-            this.findNavController().navigate(
-                ProfileFragmentDirections.actionProfileFragmentToMyOrdersFragment()
-            )
+            setContent {
+                YourWaterTheme {
+                    ProfileScreen(clientProfileViewModel = viewModel,navController = navController)
+                }
+            }
         }
         return binding.root
     }
---
+++ app/src/main/java/ru/iwater/youwater/screen/profile/ProfileMenuScreen.kt
@@ -0,0 +1,144 @@
+package ru.iwater.youwater.screen.profile
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import ru.iwater.youwater.R
+import ru.iwater.youwater.data.ClientProfileViewModel
+import ru.iwater.youwater.theme.Blue500
+import ru.iwater.youwater.theme.YouWaterTypography
+import ru.iwater.youwater.theme.YourWaterTheme
+
+@Composable
+fun ProfileScreen(clientProfileViewModel: ClientProfileViewModel = viewModel(),navController: NavController) {
+    val modifier = Modifier
+    val client by clientProfileViewModel.client.observeAsState()
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(bottom = 60.dp)) {
+        NameUser(modifier = modifier, nameUser = client?.name ?: "")
+        MenuButton(modifier = modifier, painter = painterResource(id = R.drawable.ic_orders), nameButton = stringResource(
+            id = R.string.fragment_profile_my_orders
+        ), description = "") {
+            navController.navigate(
+                ProfileFragmentDirections.actionProfileFragmentToMyOrdersFragment()
+            )
+        }
+        MenuButton(modifier = modifier, painter = painterResource(id = R.drawable.ic_info), nameButton = stringResource(
+            id = R.string.fragment_profile_my_data
+        ), description = "") {
+            navController.navigate(
+                ProfileFragmentDirections.actionProfileFragmentToUserDataFragment(false)
+            )
+        }
+        MenuButton(modifier = modifier, painter = painterResource(id = R.drawable.ic_favorite), nameButton = stringResource(
+            id = R.string.general_favorite
+        ), description = "") {
+            navController.navigate(
+                ProfileFragmentDirections.actionProfileFragmentToFavoriteFragment()
+            )
+        }
+        MenuButton(modifier = modifier, painter = painterResource(id = R.drawable.ic_address), nameButton = stringResource(
+            id = R.string.fragment_profile_addresses
+        ), description = "") {
+            navController.navigate(
+                ProfileFragmentDirections.actionProfileFragmentToAddresessFragment()
+            )
+        }
+    }
+}
+
+@Composable
+fun NameUser(modifier: Modifier,nameUser: String) {
+    Box(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = nameUser,
+            fontWeight = FontWeight.Bold,
+            style = YouWaterTypography.h5
+        )
+    }
+}
+
+@Composable
+fun MenuButton(modifier: Modifier, painter: Painter, nameButton: String, description: String, toScreen: () -> Unit) {
+    Box(
+        modifier = modifier
+            .height(60.dp)
+            .fillMaxWidth()
+            .clickable { toScreen() },
+        contentAlignment = Alignment.CenterStart
+    ){
+        Row(
+            modifier = modifier
+                .fillMaxSize()
+                .border(width = 1.dp, color = Color.LightGray)
+                .background(color = Color.White),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(
+                painter = painter,
+                contentDescription = description,
+                tint = Blue500,
+                modifier = modifier.padding(16.dp)
+            )
+            Text(
+                text = nameButton,
+                style = YouWaterTypography.h6
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun ProfileScreenPreview() {
+    YourWaterTheme {
+        val modifier = Modifier
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 60.dp)) {
+            NameUser(modifier = modifier, nameUser = "Екатерина Иванова")
+            MenuButton(modifier = modifier, painter = painterResource(id = R.drawable.ic_orders), nameButton = stringResource(
+                id = R.string.fragment_profile_my_orders
+            ), description = "") {}
+            MenuButton(modifier = modifier, painter = painterResource(id = R.drawable.ic_info), nameButton = stringResource(
+                id = R.string.fragment_profile_my_data
+            ), description = "") {}
+            MenuButton(modifier = modifier, painter = painterResource(id = R.drawable.ic_favorite), nameButton = stringResource(
+                id = R.string.general_favorite
+            ), description = "") {}
+            MenuButton(modifier = modifier, painter = painterResource(id = R.drawable.ic_address), nameButton = stringResource(
+                id = R.string.fragment_profile_addresses
+            ), description = "") {}
+            MenuButton(modifier = modifier, painter = painterResource(id = R.drawable.ic_notification), nameButton = stringResource(
+                id = R.string.general_notice
+            ), description = "") {}
+        }
+    }
+}
