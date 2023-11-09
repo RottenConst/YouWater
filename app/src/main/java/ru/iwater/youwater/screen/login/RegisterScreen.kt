d844c079e1e761b7506f53935213b682220ca01b
Konstantin <konstload@gmail.com>
2023-11-09 19:54:00 +0300

    Валидация данных

--- app/src/main/java/ru/iwater/youwater/screen/login/RegisterScreen.kt
+++ app/src/main/java/ru/iwater/youwater/screen/login/RegisterScreen.kt
@@ -3,10 +3,13 @@ package ru.iwater.youwater.screen.login
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Checkbox
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -20,6 +23,7 @@ import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.launch
 import ru.iwater.youwater.R
+import ru.iwater.youwater.theme.Blue500
 import ru.iwater.youwater.theme.YouWaterTypography
 import ru.iwater.youwater.vm.AuthViewModel
 import ru.iwater.youwater.theme.YourWaterTheme
@@ -40,16 +44,34 @@ fun RegisterScreen(
         var nameClient by rememberSaveable {
             mutableStateOf("")
         }
+        var isValidName by rememberSaveable {
+            mutableStateOf(true)
+        }
         var emailClient by rememberSaveable {
             mutableStateOf("")
+        }
+        var isValidEmail by rememberSaveable {
+            mutableStateOf(true)
         }
         var checkMessage by rememberSaveable {
             mutableStateOf(true)
         }
         LoginTitle(title = stringResource(id = R.string.fragment_register_register_text))
-        RegisterNameField(nameClient = nameClient, setNameClient = {nameClient = it})
-        RegisterEmailClient(emailClient = emailClient, setEmailClient = {emailClient = it})
-        ButtonRegisterClient(isEnabledButton = (nameClient.isNotEmpty() && emailClient.isNotEmpty())) {
+        RegisterNameField(
+            nameClient = nameClient,
+            isValidateName = isValidName
+        ) {
+            nameClient = it
+            isValidName = authViewModel.isValidName(it)
+        }
+        RegisterEmailClient(
+            emailClient = emailClient,
+            isValidateEmail = isValidEmail,
+            setEmailClient = {
+                emailClient = it
+                isValidEmail = authViewModel.isValidEmail(it)
+            })
+        ButtonRegisterClient(isEnabledButton = (nameClient.isNotEmpty() && emailClient.isNotEmpty() && isValidName && isValidEmail)) {
             authViewModel.viewModelScope.launch {
                 authViewModel.registerClient(phone, nameClient, emailClient, checkMessage, navController)
             }
@@ -66,7 +88,7 @@ fun RegisterScreen(
 }
 
 @Composable
-fun RegisterNameField(nameClient: String, setNameClient: (String) -> Unit) {
+fun RegisterNameField(nameClient: String, isValidateName: Boolean, setNameClient: (String) -> Unit) {
     OutlinedTextField(
         modifier = Modifier
             .fillMaxWidth()
@@ -76,12 +98,22 @@ fun RegisterNameField(nameClient: String, setNameClient: (String) -> Unit) {
         onValueChange =  { name ->
             setNameClient(name)
         },
-        label = { Text(text = stringResource(id = R.string.fragment_register_name_lastname))}
+        label = { Text(text = stringResource(id = R.string.fragment_register_name_lastname))},
+        isError = !isValidateName,
+        supportingText = { if (!isValidateName) Text(text = stringResource(id = R.string.support_text_edit_name)) },
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Blue500,
+            focusedContainerColor = Color.White,
+            unfocusedContainerColor = Color.White,
+            errorContainerColor = Color.White,
+            focusedLabelColor = Blue500,
+            cursorColor = Blue500
+        )
     )
 }
 
 @Composable
-fun RegisterEmailClient(emailClient: String, setEmailClient: (String) -> Unit) {
+fun RegisterEmailClient(emailClient: String, isValidateEmail: Boolean, setEmailClient: (String) -> Unit) {
     OutlinedTextField(
         modifier = Modifier
             .fillMaxWidth()
@@ -91,7 +123,17 @@ fun RegisterEmailClient(emailClient: String, setEmailClient: (String) -> Unit) {
         onValueChange = { email ->
             setEmailClient(email)
         },
-        label = { Text(text = stringResource(id = R.string.general_email))}
+        label = { Text(text = stringResource(id = R.string.general_email))},
+        isError = !isValidateEmail,
+        supportingText = { if (!isValidateEmail) Text(text = stringResource(id = R.string.support_text_edit_email)) },
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Blue500,
+            focusedContainerColor = Color.White,
+            unfocusedContainerColor = Color.White,
+            errorContainerColor = Color.White,
+            focusedLabelColor = Blue500,
+            cursorColor = Blue500
+        )
     )
 }
 
@@ -107,6 +149,7 @@ fun ButtonRegisterClient(isEnabledButton: Boolean, registerClient: () -> Unit) {
             ),
         shape = RoundedCornerShape(8.dp),
         enabled = isEnabledButton,
+        colors = ButtonDefaults.buttonColors(containerColor = Blue500),
         onClick = {
             registerClient()
         }) {
@@ -122,8 +165,13 @@ fun CheckMessage(message: String, check: Boolean, setChecked: (Boolean) -> Unit)
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Checkbox(
-            checked = check,
-            onCheckedChange = {setChecked(it)})
+            checked = !check,
+            onCheckedChange = {setChecked(it)},
+            colors = CheckboxDefaults.colors(
+                checkedColor = Blue500,
+                uncheckedColor = Color.Gray
+            )
+        )
         Text(
             text = message,
             style = YouWaterTypography.body2,
@@ -142,6 +190,12 @@ private fun InputRegisterDataPreview() {
     var emailClient by rememberSaveable {
         mutableStateOf("")
     }
+    var isValidName by remember {
+        mutableStateOf(false)
+    }
+    var isValidEmail by remember {
+        mutableStateOf(false)
+    }
     var check by rememberSaveable {
         mutableStateOf(true)
     }
@@ -153,8 +207,20 @@ private fun InputRegisterDataPreview() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             LoginTitle(title = stringResource(id = R.string.fragment_register_register_text))
-            RegisterNameField(nameClient = nameClient, setNameClient = {nameClient = it})
-            RegisterEmailClient(emailClient = emailClient, setEmailClient = {emailClient = it})
+            RegisterNameField(
+                nameClient = nameClient,
+                isValidateName = isValidName
+            ) {
+                nameClient = it
+                isValidName = it.contains(Regex("""[^A-zА-я\s]"""))
+            }
+            RegisterEmailClient(
+                emailClient = emailClient,
+                isValidateEmail = isValidEmail,
+                setEmailClient = {
+                    emailClient = it
+                    isValidEmail = it.contains(Regex("""(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})"""))
+                })
             ButtonEnter(
                 text = stringResource(id = R.string.fragment_register_finish),
                 isEnabledButton = (nameClient.isNotEmpty() && emailClient.isNotEmpty())
